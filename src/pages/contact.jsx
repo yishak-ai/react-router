1d9de28018d0b7e56333ce5e9e0f58c1334d880c
Gishen21 <teferayishak52@gmail.com>
2025-08-02 03:20:34 -0700

    commit un commited file

---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function contact() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-semibold text-blue-600">Contact Page</h1>
+    </div>
+  );
+}
+
+export default contact;
