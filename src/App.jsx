83b438de1dc4f3337ccc4260921666b4de3b14cd
Gishen21 <teferayishak52@gmail.com>
2025-08-02 03:19:11 -0700

    this is for page folder craation with file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 function App() {
-  return (
-    <div>
-      <h1 className="fint-serif font-bold text-3xl uppercase ">
-        how to learn react
-      </h1>
-    </div>
-  );
+  return <div className="flex flex-col  min-h-screen bg-gray-100"></div>;
 }
 export default App;
