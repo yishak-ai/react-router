8fe04d8a78c6116db13387da7bdbc1ed91c40659
Gishen21 <teferayishak52@gmail.com>
2025-08-02 02:42:47 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./App.css";
-
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+function App() {
+  return (
+    <div>
+      <h1 className="fint-serif font-bold text-3xl uppercase ">
+        how to learn react
+      </h1>
+    </div>
+  );
 }
+export default App;
